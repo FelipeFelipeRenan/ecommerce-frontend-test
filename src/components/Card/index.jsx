9cc25f858e174212778853f61cda8c647c940d3b
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-11 17:31:36 -0300

    Adding a button to the cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,19 @@
 import "./index.scss"
 
 
+function clicado(){
+    window.prompt("clicado")
+}
+
+
 const Card = (props) => {
 
     return(
         <div className="card">
-            {props.name}
-            {props.price}
+            <div>Item: {props.name}</div>
+
+            <div>Preço: {props.price}</div>
+            <button onClick={clicado}>Adicionar</button>
            
         </div>
     )
