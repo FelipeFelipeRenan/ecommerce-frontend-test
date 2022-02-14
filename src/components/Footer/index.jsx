d326ba12ddc77b54dc0af63b4f87f430c1b1183d
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-13 21:50:32 -0300

    Adding Links to LinkedIn and GitHub

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,10 @@ import './index.scss'
 const Footer = () =>{
     return(
         <footer>
-            <p>oi</p>      
+            <div className='footer-container'></div>
+            <div>Pagina desenvolvida por <a href='https://www.linkedin.com/in/felipe-renan/' target="_blank" rel='noreferrer'>@Felipe</a></div>     
+            <div> <i class="ri-github-fill"></i> <a href='https://github.com/FelipeFelipeRenan' target="_blank" rel='noreferrer'>@Felipe</a></div>     
+
         </footer>
     )
 }
